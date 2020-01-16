360e5e2af682adfb079ab041645c78619a6565ad
Patrick Bower <bower.patrick@gmail.com>
2020-01-16 14:11:29 +0000

    add lo dash functions

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
 import React, { useState } from 'react';
 import NationalTrustLocations from './national-trust-places.json';
+import { hasIn, mapValues, matches, filter, find, matchesProperty } from 'lodash';
 
 function App() {
 
   // const [list, setlist] = useState([]);
 
- const locations = () => {
+//  const locations = () => {
 
-  // console.log(NationalTrustLocations);
+//   // console.log(NationalTrustLocations);
 
-  const seList = [];
-  Object.keys(NationalTrustLocations).map(function(key, index) {
+//   const seList = [];
+//   Object.keys(NationalTrustLocations).map(function(key, index) {
 
-    if (NationalTrustLocations[key].cmsRegion === 'RegionLondonSouthEast' && NationalTrustLocations[key].subTitle.includes("Kent")) {
+//     if (NationalTrustLocations[key].cmsRegion === 'RegionLondonSouthEast' && NationalTrustLocations[key].subTitle.includes("Kent")) {
 
-      console.log(NationalTrustLocations[key]);
+//       console.log(NationalTrustLocations[key]);
 
-      seList.push({
-        title: NationalTrustLocations[key].title,
-        location: NationalTrustLocations[key].subTitle,
-        image: NationalTrustLocations[key].imageUrl
-      })
-    }
-    return false;
-  });
+//       seList.push({
+//         title: NationalTrustLocations[key].title,
+//         location: NationalTrustLocations[key].subTitle,
+//         image: NationalTrustLocations[key].imageUrl
+//       })
+//     }
+//     return false;
+//   });
   
-  console.log(seList);
+//   console.log(seList);
+//  }
+
+const locations = () => {
+
+  var regionLondonSouthEast = filter(NationalTrustLocations, matches({cmsRegion: "RegionLondonSouthEast"}))
+  var regionLondonSouthEastFind = find(NationalTrustLocations, matchesProperty({cmsRegion: "RegionLondonSouthEast"}));
+
+  console.log(regionLondonSouthEast);
+  console.info(regionLondonSouthEastFind);
+
+  // mapValues(NationalTrustLocations, item => {
+  //   console.log(item);
+  // })
+
  }
 
  locations();
